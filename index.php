15c3528d54d77ffa942ba589393295bdd1054406
Finn <finnmyint.dev@gmail.com>
2025-06-20 23:53:50 +0630

    Updated Design

--- index.php
+++ index.php
@@ -12,68 +12,59 @@ require 'config.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index To Do List</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <title>To Do Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
+<body class="bg-light">
 
-<?php 
-    $pdostatement = $pdo->prepare("SELECT * FROM todo ORDER BY List_id DESC");
-    $pdostatement->execute();
-    $result = $pdostatement->fetchAll();
+<div class="container mt-5">
+    <div class="d-flex justify-content-between align-items-center mb-3">
+        <h2 class="fw-bold">📝 To Do List</h2>
+        <a href="AddList.php" class="btn btn-success">+ Create Task</a>
+    </div>
 
-?>
+    <div class="card shadow-sm">
+        <div class="card-body">
 
-<div class="card">
+            <div class="table-responsive">
+                <table class="table table-striped table-hover align-middle">
+                    <thead class="table-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Created Date</th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
 
-<div class="card-body">
+                    <tbody>
+                        <?php 
+                        $i = 1;
+                        if ($result) {
+                            foreach ($result as $value) {
+                        ?>
+                        <tr>
+                            <th scope="row"><?php echo $i++; ?></th>
+                            <td><?php echo htmlspecialchars($value['Title']); ?></td>
+                            <td class="text-truncate" style="max-width: 250px;"><?php echo htmlspecialchars($value['Description']); ?></td>
+                            <td><?php echo date('Y-m-d', strtotime($value['CreatedAt'])); ?></td>
+                            <td>
+                                <a href="EditList.php?id=<?php echo $value['List_id']; ?>" class="btn btn-sm btn-warning me-1">Edit</a>
+                                <a href="Delete.php?id=<?php echo $value['List_id']; ?>" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')">Delete</a>
+                            </td>
+                        </tr>
+                        <?php 
+                            }
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
 
-    <h2>To Do Home Page</h2>
-    <a href="AddList.php" type="button" class="btn btn-success">Create</a> <br> 
-
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Title</th>
-                <th>Description</th>
-                <th>Created Date</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-
-        <tbody> 
-            <?php 
-            $i=1;
-            if($result){
-                foreach ($result as $value){
-                 ?> 
-
-                 <tr>
-                <td><?php echo $i;?></td>
-                <td><?php echo $value['Title'];?></td>
-                <td><?php echo $value['Description'];?></td>
-                <td><?php  echo date('Y-m-d',strtotime($value['CreatedAt']))?></td>
-                <td>
-   <a href="EditList.php?id=<?php  echo $value["List_id"]?>" type="button" class="btn btn-warning">Edit</a>
-
-   <a href="Delete.php?id=<?php  echo $value["List_id"]?>" type="button" class="btn btn-danger">Delete</a>
-                </td>
-            </tr>
-                 
-                 <?php 
-                 $i++;
-                }
-            }
-            ?>
-            
-        </tbody>
-
-    </table>
-
+        </div>
+    </div>
 </div>
 
-</div>
-    
 </body>
 </html>
